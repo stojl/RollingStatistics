12cef27a444b01648c7da3c9f1a67eb61f404fdb
Lucas Støjko Andersen <93032255+stojl@users.noreply.github.com>
2022-09-30 14:27:01 +0200

    RunningStatistics package
    Adds the R package skeleton and implemented functions.

---
+++ R/running_mean.R
@@ -0,0 +1,31 @@
+#' Running mean
+#'
+#' A function for creating a running mean object.
+#'
+#' @return A function that takes a numeric vector as argument
+#' @export
+#'
+#' @examples
+#' a <- c(1, 2, 3, 4, 5)
+#' b <- c(4, 2, 5, 1, 3)
+#' mu <- running_mean()
+#' mu(a) # Returns the mean of vector 'a'
+#' mu()  # Returns the current mean - the mean of vector 'a'
+#' mu(b) # Returns the mean of vector c(a, b)
+running_mean <- function() {
+  m <- 0
+  S <- 0
+
+  function(x = NULL) {
+    if(!is.null(x)) {
+      m <<- m + length(x)
+      S <<- S + sum(x)
+    }
+
+    if(m > 0) {
+      S / m
+    } else {
+      NaN
+    }
+  }
+}
